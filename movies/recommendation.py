5713b62f52adf6fe521db6de8f4a864ceb527bd2
grkm <garslan13@gmail.com>
2021-07-22 09:52:13 +0300

    recommendation.py file is updated.

--- movies/recommendation.py
+++ movies/recommendation.py
@@ -19,32 +19,28 @@ def recommender(user: get_user_model()) -> List[str]:
     rated_movies_by_user = [user_rating_obj.movie.title for user_rating_obj in user_rating_obj_set]
     ratings_by_user = [user_rating_obj.user_rating for user_rating_obj in user_rating_obj_set]
     movie_rating_data = list(zip(rated_movies_by_user, ratings_by_user))
-
     user_movie_rating_data = [{'title': _title, 'rating': _rating} for _title, _rating in movie_rating_data]
     input_movie = pd.DataFrame(user_movie_rating_data)
     # Filtering out the movies by title
-    movie_id = movie[movie['title'].isin(input_movie['title'].tolist())]
+    movie_id = movie.loc[movie['title'].isin(input_movie['title'].tolist())]
     input_movie = pd.merge(movie_id, input_movie)
     # Get users who watched the similar movies
-    users = rating[rating['movieId'].isin(input_movie['movieId'].tolist())]
-
+    users = rating.loc[rating['movieId'].isin(input_movie['movieId'].tolist())]
     user_subset_group = users.groupby(['userId'])
-    # Sort user_subset_group so that users who have similar movie tast will have higher priority
+    # Sort user_subset_group so that users who have similar movie taste will have higher priority
     user_subset_group = sorted(user_subset_group, key=lambda x: len(x[1]), reverse=True)[0:100]
     # Define Pearson Correlation dict
     pearson_cor_dict = {}
-
     for name, group in user_subset_group:
         group = group.sort_values(by='movieId')
         len_group = len(group)
         input_movie = input_movie.sort_values(by='movieId')
 
         # Review scores for the movies that they both have in common
-        temp = input_movie[input_movie['movieId'].isin(group['movieId'].tolist())]
-        # And then store them in a temporary buffer variable in a list format to facilitate future calculations
+        temp = input_movie.loc[input_movie['movieId'].isin(group['movieId'].tolist())]
         temp_rating_list = temp['rating'].tolist()
-        # put the current user group reviews in a list format
         temp_group_list = group['rating'].tolist()
+
         # Calculate the Pearson Correlation between two users
         s_xx = sum([i ** 2 for i in temp_rating_list]) - pow(sum(temp_rating_list), 2) / float(len_group)
         s_yy = sum([i ** 2 for i in temp_group_list]) - pow(sum(temp_group_list), 2) / float(len_group)
@@ -64,12 +60,10 @@ def recommender(user: get_user_model()) -> List[str]:
     pearson_df.index = range(len(pearson_df))
     top_users = pearson_df.sort_values(by='similarityIndex', ascending=False)[0:50]
     top_users_rating = top_users.merge(rating, left_on='userId', right_on='userId', how='inner')
-
     # Multiply the similarity by the ratings of users
     top_users_rating['weightedRating'] = top_users_rating['similarityIndex'] * top_users_rating['rating']
     temp_top_users_rating = top_users_rating.groupby('movieId').sum()[['similarityIndex', 'weightedRating']]
     temp_top_users_rating.columns = ['sum_similarityIndex', 'sum_weightedRating']
-
     # Define recommendation_df dict
     recommendation_df = pd.DataFrame()
     # The weighted average
@@ -79,11 +73,11 @@ def recommender(user: get_user_model()) -> List[str]:
 
     recommendation_df['movieId'] = temp_top_users_rating.index
     recommendation_df = recommendation_df.sort_values(by='weighted average recommendation score', ascending=False)
-
     # Get the first 10 recommendations to be returned
     recommendation_list = movie.loc[movie['movieId'].isin(recommendation_df.head(10)['movieId'].tolist())]
-    recommendation_list.drop(columns=['movieId'], inplace=True)
-    recommendation_list = recommendation_list.values.tolist()
-    recommendation_list = list(flatten(recommendation_list))
+    recommendation_list_new = recommendation_list.copy()
+    recommendation_list_new.drop(columns=['movieId'], inplace=True)
+    recommendation_list_new = recommendation_list_new.values.tolist()
+    recommendation_list_new = list(flatten(recommendation_list_new))
 
-    return recommendation_list
+    return recommendation_list_new
