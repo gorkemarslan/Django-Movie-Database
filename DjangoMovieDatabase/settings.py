c791f19ce0eaffb34674c8e17629534a2139f85b
gorkemarslan <garslan13@gmail.com>
2021-07-24 16:09:18 +0300

    Update settings.py

--- DjangoMovieDatabase/settings.py
+++ DjangoMovieDatabase/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import environ
-
+print(os.environ)
 # Django-environ
 env = environ.Env(
     DEBUG=(bool, False)
