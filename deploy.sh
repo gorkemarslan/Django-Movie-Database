84642a64b7d263c3f804f03cc4831280e82e11b6
grkm <garslan13@gmail.com>
2021-07-28 19:52:16 +0300

    django-testing.yml is updated.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sudo git pull
+sudo git config core.sshCommand 'ssh -i "{$PRIVATE_KEY_GITHUB_ACTIONS}"' && \
+cd Django-Movie-Database/ && \
+sudo git pull && \
 sudo docker-compose up -d --build
