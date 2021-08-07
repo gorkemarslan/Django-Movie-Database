a41a6758682366312593d06748faf12914e0db3b
grkm <garslan13@gmail.com>
2021-08-07 11:09:26 +0300

    my-stars/ is renamed as stars/

--- movies/urls.py
+++ movies/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('movies/', MovieListView.as_view(), name='movie_list'),
     path('movies/<uuid:pk>', MovieDetailView.as_view(), name='movie_detail'),
     path('recommendation/', MovieRecommendationView.as_view(), name='recommendation'),
-    path('my-stars/', UserStarsListView.as_view(), name='user_stars_list'),
+    path('stars/', UserStarsListView.as_view(), name='user_stars_list'),
 ]
