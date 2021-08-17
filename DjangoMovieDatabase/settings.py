3a2f3471f14ab12c6ab473d781cd041c4ccc4373
grkm <garslan13@gmail.com>
2021-08-17 17:32:28 +0300

    Added static_root

--- DjangoMovieDatabase/settings.py
+++ DjangoMovieDatabase/settings.py
@@ -139,7 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+if eval(env('DEBUG')):
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
